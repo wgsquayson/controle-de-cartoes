a190cd5ae62a07422588857e012874bcd12ff75a
William Quayson <wgsquayson@gmail.com>
2023-02-13 22:15:27 -0300

    fix: eslint issues

--- apps/nextjs/.eslintrc.cjs
+++ apps/nextjs/.eslintrc.cjs
@@ -1,6 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: "next/core-web-vitals",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
 };
 
 module.exports = config;
